dc71e966a793416dd65a7a3daf3f1abfd7dc7cba
Dhaval pathak <93087161+Dhaval-pathak@users.noreply.github.com>
2023-09-14 23:36:21 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:untitled/theme/colors.dart';
 import 'package:flutter/material.dart';
-import 'ChooseLogin.dart';
-void main() async {
+import 'pages/home.dart';
+
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp();
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChooseLogin(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: primary,
+      ),
+      home: Home(),
     );
   }
 }
